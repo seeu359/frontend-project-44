4078ae40ddfd1fd92ca9b3e8394d6afe9bb57950
a.cheremushkin <a.ch3re359@yandex.ru>
2023-09-16 16:08:14 +0300

    Fix

--- games/brain-even.js
+++ games/brain-even.js
@@ -5,8 +5,8 @@ export const gameRules = 'Answer "yes" if the number is even, otherwise answer "
 const isEvenNumber = (number) => number % 2 === 0;
 
 export const brainEven = () => {
-	const randomNumber = getRandomNumber(100);
-	const question = `Question: ${randomNumber}`;
-	const correctAnswer = (isEvenNumber(randomNumber)) ? 'yes' : 'no';
-	return [question, correctAnswer];
+  const randomNumber = getRandomNumber(100);
+  const question = `Question: ${randomNumber}`;
+  const correctAnswer = (isEvenNumber(randomNumber)) ? 'yes' : 'no';
+  return [question, correctAnswer];
 };
